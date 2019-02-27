44c59b7d03f9d88e6b0ea7eed247a8bc4183a57b
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-02-27 15:01:19 -0800

    moved makeContactRow to own file

--- test/contacts-component.test.js
+++ test/contacts-component.test.js
@@ -1,25 +1,5 @@
+import { makeContactRow } from '../src/contacts-component.js';
 const test = QUnit.test;
-
-function makeContactRow(contact) {
-    const currencyStyle = { style: 'currency', currency: 'USD' };
-    const html = /*html*/
-    `<tr>
-        <td>${contact.isActive ? 'Yes' : 'No'}</td>
-        <td>${contact.firstName}</td>
-        <td>${contact.lastName}</td>
-        <td class="center"><img src="${contact.picture}"></td>
-        <td>${contact.balance.toLocaleString('en-US', currencyStyle)}</td>
-        <td>${contact.age}</td>
-        <td title="${contact.about}">${contact.company}</td>
-        <td>${contact.email}</td>
-        <td class="capitalize">${contact.favoriteFruit}</td>
-    </tr>`;
-
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content;
-}
-
 
 test('makes contact row template', assert => {
     // arrange
